962d505c09e15ff3a0fe3a72c2b14c61b7b2176d
Steven Chavez <schavez256@yahoo.com>
2017-01-25 18:59:18 -0700

    created product class and class fields

---
+++ php/classes/product.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * PDO enabled container for product authentication
+ *
+ * Author: Steven
+ * Date: 1/25/2017
+ * Time: 6:51 PM
+ * Version: 1.0
+ */
+
+class Product
+{
+    private $productID;
+    private $prodcutName;
+    private $productNumber;
+    private $color;
+    private $numPerCase;
+    private $typeID;
+    private $brandNew;
+}
+?>
