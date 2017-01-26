b1316b0932fe245e3a488f32a0fcab332478694c
Steven Chavez <schavez256@yahoo.com>
2017-01-25 17:33:09 -0700

    Added basic form to add products

---
+++ product-add.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Product Insert</title>
+</head>
+<body>
+    <form>
+        Product Name: <br>
+        <input type="text" name="ProductName"><br>
+        Product Number: <br>
+        <input type="text" name="ProductNumber"><br>
+        Color: <br>
+        <input type="text" name="Color"><br>
+        Number Per Case: <br>
+        <input type="number" name="NumberPerCase"><br>
+        Type: <br>
+        <input type="number" name="TypeID"><br>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
