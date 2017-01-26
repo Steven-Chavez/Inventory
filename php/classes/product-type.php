aae62289a6321e3ae149decfae2ed7c75913c1d8
Steven Chavez <schavez256@yahoo.com>
2017-01-26 13:37:14 -0700

    added ProductType class to php\class\

---
+++ php/classes/product-type.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * PDO enabled container for product type authentication
+ * Author: Steven
+ * Date: 1/26/2017
+ * Time: 1:28 PM
+ * Version: 1.0
+ */
+
+class ProductType
+{
+    //###################################
+    //  FIELDS
+    //###################################
+    /**
+     * @var id for product type
+     */
+    private $typeId;
+    /**
+     * @var name of product type
+     */
+    private $typeName;
+
+    //###################################
+    //  CONSTRUCTOR
+    //###################################
+
+    function __construct($typeId, $typeName)
+    {
+        $this->setTypeId($typeId);
+        $this->setTypeName($typeName);
+    }
+
+    //###################################
+    //  ACCESSOR METHODS
+    //###################################
+
+    /**
+     * get id of product type
+     * @return mixed
+     */
+    public function getTypeId()
+    {
+        return $this->typeId;
+    }
+
+    /**
+     * get name of product type
+     * @return mixed
+     */
+    public function getTypeName()
+    {
+        return $this->typeName;
+    }
+
+
+    //###################################
+    //  MUTATOR METHODS
+    //###################################
+
+    /**
+     * Sets product type id
+     * @param mixed $typeId
+     */
+    public function setTypeId($typeId)
+    {
+        $this->typeId = $typeId;
+    }
+
+    /**
+     * Sets product type name
+     * @param mixed $typeName
+     */
+    public function setTypeName($typeName)
+    {
+        $this->typeName = $typeName;
+    }
+}
