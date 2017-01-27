fa1973faaae2877682a2edb33ee54395873928af
Steven Chavez <schavez256@yahoo.com>
2017-01-26 20:26:45 -0700

    updated aesthetics of form and added input for bolean

--- product-add.php
+++ product-add.php
@@ -16,13 +16,16 @@
 <body>
     <form>
         Product Name: <br>
-        <input type="text" name="ProductName"><br>
+        <input type="text" name="ProductName"><br><br>
         Product Number: <br>
-        <input type="text" name="ProductNumber"><br>
+        <input type="text" name="ProductNumber"><br><br>
         Color: <br>
-        <input type="text" name="Color"><br>
+        <input type="text" name="Color"><br><br>
         Number Per Case: <br>
-        <input type="number" name="NumberPerCase"><br>
+        <input type="number" name="NumberPerCase"><br><br>
+        In Box?: <br>
+        <input type="radio" name="BrandNew" value="Yes" checked> Yes<br>
+        <input type="radio" name="BrandNew" value="No"> No <br><br>
         Type: <br>
         <select name="TypeId">
             <?php
