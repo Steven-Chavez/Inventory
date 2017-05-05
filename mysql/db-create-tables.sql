c66c1d9c1c11044d5e5cd6e38f31a6accec5795a
Steven Chavez <schavez256@yahoo.com>
2017-05-05 17:02:12 -0600

    Added create table for InventoryLocations

--- mysql/db-create-tables.sql
+++ mysql/db-create-tables.sql
@@ -6,20 +6,26 @@
 
 -- Describes the type of product (i.e. 
 -- cardboard, equipment, pos).
-CREATE TABLE ProductType
+CREATE TABLE ProductTypes
 (
     TypeId int NOT NULL AUTO_INCREMENT,
     TypeName varchar(30) UNIQUE,
     PRIMARY KEY(TypeId)
 );
 
-CREATE TABLE Location
+-- Describes location of work site (i.e.
+-- warehouse, p.e.c., bin)
+CREATE TABLE Locations
 (
     LocationId int NOT NULL AUTO_INCREMENT,
+    Address varchar (40) NOT NULL,
     City varchar(30) NOT NULL,
     LocationState varchar(30) NOT NULL,
+    ZipCode varchar(10) NOT NULL,
     Zone varchar(30) NOT NULL,
     Region varchar(30) NOT NULL,
     PRIMARY KEY(LocationId)
+);
+
 );
 
