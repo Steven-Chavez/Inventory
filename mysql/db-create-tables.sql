629bd29af7e464afe30ca84ea8f9e1d61c43c985
Steven Chavez <schavez256@yahoo.com>
2017-05-05 16:44:23 -0600

    Added create table for location

--- mysql/db-create-tables.sql
+++ mysql/db-create-tables.sql
@@ -4,10 +4,22 @@
  * File: db-create-tables.sql
  */
 
+-- Describes the type of product (i.e. 
+-- cardboard, equipment, pos).
 CREATE TABLE ProductType
 (
     TypeId int NOT NULL AUTO_INCREMENT,
     TypeName varchar(30) UNIQUE,
     PRIMARY KEY(TypeId)
+);
+
+CREATE TABLE Location
+(
+    LocationId int NOT NULL AUTO_INCREMENT,
+    City varchar(30) NOT NULL,
+    LocationState varchar(30) NOT NULL,
+    Zone varchar(30) NOT NULL,
+    Region varchar(30) NOT NULL,
+    PRIMARY KEY(LocationId)
 );
 
