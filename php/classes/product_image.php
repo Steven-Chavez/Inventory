171944e7cd99951d9a231a00597002590faddd6a
Steven Chavez <schavez256@yahoo.com>
2017-05-10 21:40:21 -0600

    Added Fields and Mutator methods to class

--- php/classes/product_image.php
+++ php/classes/product_image.php
@@ -15,7 +15,23 @@ class Image
     //###################################
     //  FIELDS
     //###################################
-
+    /**
+     * @var id for product image. Primary key. 
+     */
+    private $id;
+    /**
+     * @var type of image (i.e. jpg, gif, png) 
+     */
+    private $type;
+    /**
+     * @var name of image file 
+     */
+    private $name;
+    /**
+     * @var url of product image
+     */
+    private $url;
+    
     //###################################
     //  CONSTRUCTOR
     //###################################
@@ -27,7 +43,42 @@ class Image
     //###################################
     //  MUTATOR METHODS
     //###################################
-  
+    /**
+     * set product image id
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    
+    /**
+     * set image type
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+    
+    /**
+     * set image file name
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    
+    /**
+     * set URL for image location
+     * @param string $url
+     */
+    public function setURL($url)
+    {
+        $this->url = $url;
+    }        
+            
     //###################################
     //  DB CRUD METHODS
     //###################################
