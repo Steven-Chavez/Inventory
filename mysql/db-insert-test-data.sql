94971d0d5752778534b8d193772509df659ae9d0
Steven Chavez <schavez256@yahoo.com>
2017-06-24 14:37:33 -0600

    Created .sql to insert test data into db

---
+++ mysql/db-insert-test-data.sql
@@ -0,0 +1,11 @@
+/**
+ * Author:  Steven Chavez
+ * Created: June 24, 2017
+ * File: db-insert-test-data.sql
+ * Version: 1.0
+ *
+ * Inserts test data to for developing purposes.
+ * All data is based off real products but not
+ * real inventories. 
+ */
+
