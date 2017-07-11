22adabc16755821dfe170842e211a384ce574d47
Steven Chavez <schavez256@yahoo.com>
2017-07-11 15:55:21 -0600

    Updated index.php for new design

--- index.php
+++ index.php
@@ -7,29 +7,14 @@
      * Version: 1.0
      */
     require_once("../../database.php");
-    require_once("php/classes/inventory.php");
-
+    require_once("php/classes/location.php");
 
     // connect to database and obtain PDO object
     $pdo = new DatabaseConnect();
     $pdo = $pdo->getPDO();
-
-    session_start();
     
-    if(!isset($_SESSION["locationId"]))
-    {
-        //set default inventory date index
-        $_SESSION["locationId"] = 1;
-    }
-    
-    
-    //$dates = Inventory::readInventoryDates($pdo);
-    //$types = ProductType::readType($pdo);
-    $joinByType = Inventory::readInventoryByLocation($pdo, $_SESSION["locationId"]);
-    
-    // format current date for better presentation
-    $dateStr = $joinByType[0]->InventoryDate;
-    $CardboardInvCurrentDate= date("F j, Y", strtotime($dateStr));
+    // Get list of locations to choose from.
+    $location = Location::readLocationCityAndState($pdo);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -67,56 +52,32 @@
         </div><!-- /.container-fluid -->
     </nav>
     <div class="row">
-        <aside  style="padding-left: 2em" class="col-md-2">
-            
-        </aside>
-        <main class="col-md-8">
-            <div class="table-responsive">
-                <h4>
-                    Most Current Cardboard Inventory: 
-                    <?php echo $CardboardInvCurrentDate; ?> 
-                </h4>
-                
-                    <?php
-                    
-                    $total = count($joinByType);
-                    $index = 1;
-                    $header = false;
-                    
-                    for($i = 0; $i < $total; $i++)
-                    {
-                        if($joinByType[$i]->InventoryLocationId == $index && $header == false)
-                        {
-                            echo '  <table class="table table-bordered">
-                                    <tr>
-                                        <th>Product Name</th>
-                                        <th>EQI</th>
-                                        <th>Color</th>
-                                        <th>Qty</th>
-                                    </tr>';
-                            $header = true;
-                        }
-                        else if($joinByType[$i]->InventoryLocationId != $index)
-                        {
-                            $header = false;
-                            $index++;
-                        }
-                        
-                        echo "
-                                <tr>
-                                    <td>{$joinByType[$i]->ProductName}</td>
-                                    <td>{$joinByType[$i]->ProductNumber}</td>
-                                    <td>{$joinByType[$i]->Color}</td>
-                                    <td>{$joinByType[$i]->Quantity}</tb>
-                                </tr> 
-                            ";
-                            
-                    }
-                        session_unset();
-                        session_destroy();
-                    ?>
-                </table>
-            </div>
+        <header class="col-md-6 col-md-offset-3">
+            <h1>Welcome To E-C Inventory!</h1>
+            <p>
+                E-C Inventory is an inventory control system for equipment and cardboard.
+                Please select the location to see current and past inventories. Further
+                filtering of inventory information can be accomplished with the controls
+                on the side after selecting your location. 
+            </p>
+        </header>
+    </div>
+    <div class="row">
+        <main class="col-md-6 col-md-offset-3">
+            <form  action="php/form-processor/select-inventory-location-processor.php" method="post">
+                <h3>Select Location:</h3> <br>
+            <select class="form-control" name="locationId">
+                <?php
+                //Populate drop down box with values from db
+                foreach ($location as $value)
+                {
+                    echo"<option value=\"{$value->LocationId}\">{$value->City}, 
+                            {$value->LocationState}</option>";
+                }
+                ?>
+            </select><br>
+            <input type="submit" value="Submit">
+        </form>
         </main>
     </div>
 </body>
