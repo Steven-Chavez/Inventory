110460245431d269fc357ab15e3eb9d6300bbe9c
Steven Chavez <schavez256@yahoo.com>
2017-07-18 14:16:07 -0600

    Added form to handle a product search

--- index.php
+++ index.php
@@ -82,6 +82,19 @@
                 </select><br>
                 <input type="submit" value="Submit">
             </form>
+            <form action="php/form-processor/product-search-processor.php" method="post"><br>
+                <h3>Search For Product</h3> <br>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search for...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="Submit">Search</button>
+                            </span>
+                        </div><!-- /input-group -->
+                    </div><!-- /.col-lg-6 -->
+                </div><!-- /.row -->
+            </form>
         </main>
     </div>
 </body>
