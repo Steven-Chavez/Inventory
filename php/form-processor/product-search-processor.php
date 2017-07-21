c9533b7e3bacce2ae1fb8127608aee3ce9d4bab3
Steven Chavez <schavez256@yahoo.com>
2017-07-20 22:10:05 -0600

    Created processor to handle to user search data

--- php/form-processor/product-search-processor.php
+++ php/form-processor/product-search-processor.php
@@ -7,3 +7,19 @@
  * Time: 2:17PM 
  * Version: 1.0
  */
+
+// Require appropriate files.
+require_once("../../../../database.php");
+require_once("../classes/product.php");
+
+// Connect to DB and get PDO object.
+$pdo = new DatabaseConnect();
+$pdo = $pdo->getPDO();
+
+// Initialize variables with $POST values.
+$search = $_POST['search'];
+
+// Search for product and get results.
+$results = Product::searchProduct($pdo, $search);
+
+var_dump($results);
