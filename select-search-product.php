a026ddab1fe28975af21dad462eebe9fee95f826
Steven Chavez <schavez256@yahoo.com>
2017-07-23 04:31:50 -0600

    Added webpage layout

--- select-search-product.php
+++ select-search-product.php
@@ -9,3 +9,49 @@
  * Time: 4:19AM
  * Version: 1.0
  */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Select Product</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/inventoryStyle.css">
+</head>
+<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">E-C Inventory</a>
+            </div>
+        </div><!-- /.container-fluid -->
+    </nav>
+    <div style="padding-left: 2em; padding-right: 2em" class="row">
+        <header class="col-md-6 col-md-offset-3">
+            <h1>There are multiple search results!</h1>
+            <p>
+                Please select the exact product you are looking for. 
+            </p>
+        </header>
+    </div>        
+</body>
+</html>
