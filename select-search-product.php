42ce2b7fe627e4898c069fd163c34ed534cc790e
Steven Chavez <schavez256@yahoo.com>
2017-07-23 04:24:01 -0600

    Created webpage to select product from multiple search results

---
+++ select-search-product.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This web page allows you to select from multiple search
+ * products. If there is only one search result this web page
+ * is skipped. 
+ *
+ * Author: Steven Chavez
+ * Date: 7/23/2017
+ * Time: 4:19AM
+ * Version: 1.0
+ */
